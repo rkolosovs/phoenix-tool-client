2eb3c9930b6bc1f09b788381290bd7a0cce06971
pspieler <p.spieler90@web.de>
2016-06-30 12:22:09 +0200

    mergestuff

--- armies/armyCoordinates.js
+++ armies/armyCoordinates.js
@@ -30,3 +30,40 @@ function armyCoordinates(army, coordX, coordY) {
         } else {return "not yet implemented";}
     }
 }
+
+function showHex(positionX, positionY) {
+    this.id = function(){
+		//TODO: GroßhexKleinhex Zahl bestimmen.
+	}
+    this.x = positionX;
+    this.y = positionY;
+	// reihenfolge NW,NO,O,SO,SW,W, 0 heißt kein fluss, 1 heißt fluss
+    this.fluesse = function() {
+		var flussAcc = [0,0,0,0,0,0];
+		var surroundings = this.neighbors();
+        for (var i = 0; i < rivers.length; i++) {
+			var river = rivers[i];
+			if((this.x == river[1][1] && this.y == river[1][2]) || (this.x == river[2][1] && this.y == river[2][2])){
+				for(var j = 0; j < surroundings.length; j++){
+					if((surroundings[j][1] == river[1][1] && surroundings[j][2] == river[1][2]) || (surroundings[j][1] == river[2][1] && surroundings[j][2] == river[2][2])){
+						flussAcc[j] = 1;
+					}
+				}
+			}
+    	}
+		return flussAcc;
+	}
+	this.fieldType = function(){
+		for (var i = 0; i < fields.length; i++) {
+			if((fields[i].x == this.x) && (fields[i].y == this.y)){return fields[i].type;}
+		}
+	}
+	this.neighbors = function(){
+		//reihenfolge NW,NO,O,SO,SW,W
+		if(this.y % 2 == 0){
+			return [[this.x,this.y-1],[this.x+1,this.y-1],[this.x+1,this.y],[this.x+1,this.y+1],[this.x,this.y+1],[this.x-1,this.y]];
+		} else {
+			return [[this.x-1,this.y-1],[this.x,this.y-11],[this.x+1,this.y],[this.x,this.y+1],[this.x-1,this.y+1],[this.x-1,this.y]];
+		}
+	}
+}
