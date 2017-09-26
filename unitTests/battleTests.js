72f5caa3c08594101a26a55fc84cad47792167c8
Bobby <rk@mbits.solutions>
2017-09-26 12:36:13 +0200

    Basic infrastructure for proper unit tests.
    Unit test suite using QUnit (JQuery unit test framework).
    Basic test examples for reference.

---
+++ unitTests/battleTests.js
@@ -0,0 +1,8 @@
+QUnit.module( "Function schlacht armyInteraction.js" );
+QUnit.test( "a basic test example", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
+
+QUnit.test( "a basic test example 2", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
