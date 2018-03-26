2f3883ebbc88eee33fd6ee555e03e5fb9e2f0c08
Roberts Kolosovs <rk@mbits.solutions>
2018-03-26 14:22:42 +0200

    Refactored the directionalTerrainTests test cases.
    Now using new structure of armies and battle handlers.

--- unitTests/battleTests/directionalTerrainTests.ts
+++ unitTests/battleTests/directionalTerrainTests.ts
@@ -1,158 +1,177 @@
 import {attackerArmies, defenderArmies} from "../battleTests";
 import {QUnit} from "qunit";
+import {Direction} from "../../map/direction";
+import {BattleHandler} from "../../armies/battleHandler";
+import {BattleResult, Result} from "../../armies/battleResult";
+import {GameState} from "../../gameState";
 
 const { test } = QUnit;
 
 export function directionalTerrainBattleTests() {
     test( "Attack onto a street.", function(t) {
-        attackerArmies[18].x = 9;
-        attackerArmies[18].y = 8;
-        attackerArmies[18].oldX = 9;
-        attackerArmies[18].oldY = 7;
-        defenderArmies[18].x = 9;
-        defenderArmies[18].y = 8;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 9, 8);
-        t.resultEquals( battle.result(10, 10), {victor: 'attacker', attackerLosses: [818.18], defenderLosses: [1100]} );
+        attackerArmies[18].changePosition([9, 7]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.SE);
+        attackerArmies[18].move(Direction.SE);
+        defenderArmies[18].changePosition([9, 8]);
+        let battleHandler: BattleHandler = new BattleHandler([], [9, 8]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [9, 8], 10, 10),
+            new BattleResult(Result.ATTACKER_VICTORY, [818.18], [1100]));
     });
     test( "Attack out of a forest.", function(t) {
-        attackerArmies[18].x = 9;
-        attackerArmies[18].y = 9;
-        attackerArmies[18].oldX = 10;
-        attackerArmies[18].oldY = 9;
-        defenderArmies[18].x = 9;
-        defenderArmies[18].y = 9;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 9, 9);
-        t.resultEquals( battle.result(10, 10), {victor: 'attacker', attackerLosses: [818.18], defenderLosses: [1100]} );
+        attackerArmies[18].changePosition([10, 9]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.W);
+        attackerArmies[18].move(Direction.W);
+        defenderArmies[18].changePosition([9, 9]);
+        let battleHandler: BattleHandler = new BattleHandler([], [9, 9]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [9, 9], 10, 10),
+            new BattleResult(Result.ATTACKER_VICTORY, [818.18], [1100]));
     });
     test( "Attack into a swamp.", function(t) {
-        attackerArmies[18].x = 10;
-        attackerArmies[18].y = 10;
-        attackerArmies[18].oldX = 9;
-        attackerArmies[18].oldY = 10;
-        defenderArmies[18].x = 10;
-        defenderArmies[18].y = 10;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 10, 10);
-        t.resultEquals( battle.result(10, 10), {victor: 'attacker', attackerLosses: [818.18], defenderLosses: [1100]} );
+        attackerArmies[18].changePosition([9, 10]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([10, 10]);
+        let battleHandler: BattleHandler = new BattleHandler([], [10, 10]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [10, 10], 10, 10),
+            new BattleResult(Result.ATTACKER_VICTORY, [818.18], [1100]));
     });
     test( "Attack into a desert.", function(t) {
-        attackerArmies[18].x = 11;
-        attackerArmies[18].y = 11;
-        attackerArmies[18].oldX = 10;
-        attackerArmies[18].oldY = 11;
-        defenderArmies[18].x = 11;
-        defenderArmies[18].y = 11;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 11, 11);
-        t.resultEquals( battle.result(10, 10), {victor: 'attacker', attackerLosses: [818.18], defenderLosses: [1100]} );
+        attackerArmies[18].changePosition([10, 11]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([11, 11]);
+        let battleHandler: BattleHandler = new BattleHandler([], [11, 11]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [11, 11], 10, 10),
+            new BattleResult(Result.ATTACKER_VICTORY, [818.18], [1100]));
     });
     test( "Attack downhill.", function(t) {
-        attackerArmies[18].x = 9;
-        attackerArmies[18].y = 9;
-        attackerArmies[18].oldX = 8;
-        attackerArmies[18].oldY = 9;
-        defenderArmies[18].x = 9;
-        defenderArmies[18].y = 9;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 9, 9);
-        t.resultEquals( battle.result(10, 10), {victor: 'attacker', attackerLosses: [818.18], defenderLosses: [1100]} );
+        attackerArmies[18].changePosition([8, 9]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.W);
+        attackerArmies[18].move(Direction.W);
+        defenderArmies[18].changePosition([9, 9]);
+        let battleHandler: BattleHandler = new BattleHandler([], [9, 9]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [9, 9], 10, 10),
+            new BattleResult(Result.ATTACKER_VICTORY, [818.18], [1100]));
     });
     test( "Defense downhill (attack uphill).", function(t) {
-        attackerArmies[18].x = 8;
-        attackerArmies[18].y = 8;
-        attackerArmies[18].oldX = 8;
-        attackerArmies[18].oldY = 9;
-        defenderArmies[18].x = 8;
-        defenderArmies[18].y = 8;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 8, 8);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1100], defenderLosses: [818.18]} );
+        attackerArmies[18].changePosition([8, 9]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.SW);
+        attackerArmies[18].move(Direction.SW);
+        defenderArmies[18].changePosition([8, 8]);
+        let battleHandler: BattleHandler = new BattleHandler([], [8, 8]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [8, 8], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1100], [818.18]));
     });
     test( "Defense behind a river.", function(t) {
-        attackerArmies[18].x = 8;
-        attackerArmies[18].y = 8;
-        attackerArmies[18].oldX = 9;
-        attackerArmies[18].oldY = 7;
-        defenderArmies[18].x = 8;
-        defenderArmies[18].y = 8;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 8, 8);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1250], defenderLosses: [720]} );
+        attackerArmies[18].changePosition([9, 7]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.SW);
+        attackerArmies[18].move(Direction.SW);
+        defenderArmies[18].changePosition([8, 8]);
+        let battleHandler: BattleHandler = new BattleHandler([], [8, 8]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [8, 8], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1250], [720]));
     });
     test( "Defense behind a bridge.", function(t) {
-        attackerArmies[18].x = 8;
-        attackerArmies[18].y = 8;
-        attackerArmies[18].oldX = 8;
-        attackerArmies[18].oldY = 7;
-        defenderArmies[18].x = 8;
-        defenderArmies[18].y = 8;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 8, 8);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1150], defenderLosses: [782.61]} );
+        attackerArmies[18].changePosition([8, 7]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.SE);
+        attackerArmies[18].move(Direction.SE);
+        defenderArmies[18].changePosition([8, 8]);
+        let battleHandler: BattleHandler = new BattleHandler([], [8, 8]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [8, 8], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1150], [782.61]));
     });
     test( "Defense behind a wall.", function(t) {
-        attackerArmies[18].x = 8;
-        attackerArmies[18].y = 8;
-        attackerArmies[18].oldX = 7;
-        attackerArmies[18].oldY = 8;
-        defenderArmies[18].x = 8;
-        defenderArmies[18].y = 8;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 8, 8);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1250], defenderLosses: [720]} );
+        attackerArmies[18].changePosition([7, 8]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([8, 8]);
+        let battleHandler: BattleHandler = new BattleHandler([], [8, 8]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [8, 8], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1250], [720]));
     });
     test( "Defense in own caste.", function(t) {
-        attackerArmies[18].x = 3;
-        attackerArmies[18].y = 3;
-        attackerArmies[18].oldX = 2;
-        attackerArmies[18].oldY = 3;
-        defenderArmies[18].x = 3;
-        defenderArmies[18].y = 3;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 3, 3);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1500], defenderLosses: [600]} );
+        attackerArmies[18].changePosition([2, 3]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([3, 3]);
+        let battleHandler: BattleHandler = new BattleHandler([], [3, 3]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [3, 3], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1500], [600]));
     });
     test( "Defense in own city.", function(t) {
-        attackerArmies[18].x = 4;
-        attackerArmies[18].y = 4;
-        attackerArmies[18].oldX = 3;
-        attackerArmies[18].oldY = 4;
-        defenderArmies[18].x = 4;
-        defenderArmies[18].y = 4;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 4, 4);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [2000], defenderLosses: [450]} );
+        attackerArmies[18].changePosition([3, 4]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([4, 4]);
+        let battleHandler: BattleHandler = new BattleHandler([], [4, 4]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [4, 4], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [2000], [450]));
     });
     test( "Defense in own fortress.", function(t) {
-        attackerArmies[18].x = 5;
-        attackerArmies[18].y = 5;
-        attackerArmies[18].oldX = 4;
-        attackerArmies[18].oldY = 5;
-        defenderArmies[18].x = 5;
-        defenderArmies[18].y = 5;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 5, 5);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [2500], defenderLosses: [360]} );
+        attackerArmies[18].changePosition([4, 5]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([5, 5]);
+        let battleHandler: BattleHandler = new BattleHandler([], [5, 5]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [5, 5], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [2500], [360]));
     });
     test( "Defense in own capital.", function(t) {
-        attackerArmies[18].x = 6;
-        attackerArmies[18].y = 6;
-        attackerArmies[18].oldX = 5;
-        attackerArmies[18].oldY = 6;
-        defenderArmies[18].x = 6;
-        defenderArmies[18].y = 6;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 6, 6);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [3000], defenderLosses: [300]} );
+        attackerArmies[18].changePosition([5, 6]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([6, 6]);
+        let battleHandler: BattleHandler = new BattleHandler([], [6, 6]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [6, 6], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [3000], [300]));
     });
     test( "Defense in own capital fortress.", function(t) {
-        attackerArmies[18].x = 7;
-        attackerArmies[18].y = 7;
-        attackerArmies[18].oldX = 6;
-        attackerArmies[18].oldY = 7;
-        defenderArmies[18].x = 7;
-        defenderArmies[18].y = 7;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 7, 7);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [3500], defenderLosses: [257.14]} );
+        attackerArmies[18].changePosition([6, 7]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([7, 7]);
+        let battleHandler: BattleHandler = new BattleHandler([], [7, 7]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [7, 7], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [3500], [257.14]));
     });
     test( "Defense in foreign production building.", function(t) {
-        attackerArmies[18].x = 4;
-        attackerArmies[18].y = 4;
-        attackerArmies[18].oldX = 3;
-        attackerArmies[18].oldY = 4;
-        defenderArmies[18].x = 4;
-        defenderArmies[18].y = 4;
-        defenderArmies[18].owner = 3;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 4, 4);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1250], defenderLosses: [720]} );
+        attackerArmies[18].changePosition([3, 4]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([4, 4]);
+        defenderArmies[18].owner = GameState.realms[2];
+        let battleHandler: BattleHandler = new BattleHandler([], [4, 4]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [4, 4], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1250], [720]));
     });
 }
