92534ffdcf17440fc218c20853539808e2d48adb
Roberts Kolosovs <rk@mbits.solutions>
2018-04-08 10:16:53 +0200

    Refactored test cases in landBattleTests.

--- unitTests/battleTests/landBattleTests.js
+++ unitTests/battleTests/landBattleTests.js
@@ -2,61 +2,62 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const battleTests_1 = require("../battleTests");
 const qunit_1 = require("qunit");
+const battleHandler_1 = require("../../armies/battleHandler");
+const battleResult_1 = require("../../armies/battleResult");
+const gameState_1 = require("../../gameState");
 const { test } = qunit_1.QUnit;
 function landBattleTests() {
     test("Minimal armies, defenders win by dice roll.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], 0, 0);
-        t.resultEquals(battle.result(1, 10), { victor: 'defender', attackerLosses: [1045], defenderLosses: [861.24] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], [0, 0], 1, 10), new battleResult_1.BattleResult(2 /* DEFENDER_VICTORY */, [1045], [861.24]));
     });
     test("Minimal armies, attackers win by dice roll.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], 0, 0);
-        t.resultEquals(battle.result(9, 2), { victor: 'attacker', attackerLosses: [869.57], defenderLosses: [1035] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], [0, 0], 9, 2), new battleResult_1.BattleResult(0 /* ATTACKER_VICTORY */, [869.57], [1035]));
     });
     test("Minimal armies, tie (both loose).", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], 0, 0);
-        t.resultEquals(battle.result(5, 5), { victor: 'tie', attackerLosses: [1000], defenderLosses: [1000] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], [0, 0], 5, 5), new battleResult_1.BattleResult(4 /* TIE */, [1000], [1000]));
     });
     test("Different officer count, different rolls, tie (both loose).", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[1]], [], [], 0, 0);
-        t.resultEquals(battle.result(7, 3), { victor: 'tie', attackerLosses: [1000], defenderLosses: [1000] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[0]], [battleTests_1.defenderArmies[0]], [], [], [0, 0], 7, 3), new battleResult_1.BattleResult(4 /* TIE */, [1000], [1000]));
     });
     test("Riders vs footmen on plains.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[1]], [battleTests_1.defenderArmies[8]], [], [], 0, 0);
-        t.resultEquals(battle.result(6, 6), { victor: 'defender', attackerLosses: [17000], defenderLosses: [5294.12] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[1]], [battleTests_1.defenderArmies[8]], [], [], [0, 0], 6, 6), new battleResult_1.BattleResult(2 /* DEFENDER_VICTORY */, [17000], [5294.12]));
     });
     test("Riders vs footmen in woods.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[1]], [battleTests_1.defenderArmies[8]], [], [], 1, 1);
-        t.resultEquals(battle.result(4, 4), { victor: 'attacker', attackerLosses: [5294.12], defenderLosses: [17000] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [1, 1]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[1]], [battleTests_1.defenderArmies[8]], [], [], [1, 1], 4, 4), new battleResult_1.BattleResult(0 /* ATTACKER_VICTORY */, [5294.12], [17000]));
     });
     test("Mixed armies of different compositions on plains.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[2], battleTests_1.attackerArmies[7]], [battleTests_1.defenderArmies[2], battleTests_1.defenderArmies[7]], [], [], 0, 0);
-        t.resultEquals(battle.result(5, 5), { victor: 'attacker', attackerLosses: [5456.25, 8780.49], defenderLosses: [22687.5, 4395.6] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[2], battleTests_1.attackerArmies[7]], [battleTests_1.defenderArmies[2], battleTests_1.defenderArmies[7]], [], [], [0, 0], 5, 5), new battleResult_1.BattleResult(0 /* ATTACKER_VICTORY */, [5456.25, 8780.49], [22687.5, 4395.6]));
     });
     test("Tie with soldier count, officer count and dice roll all different.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[3]], [battleTests_1.defenderArmies[3]], [], [], 0, 0);
-        t.resultEquals(battle.result(1, 10), { victor: 'tie', attackerLosses: [1205], defenderLosses: [995.85] });
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[3]], [battleTests_1.defenderArmies[3]], [], [], [0, 0], 1, 10), new battleResult_1.BattleResult(4 /* TIE */, [1205], [995.85]));
     });
     test("Combat in attackers home terrain.", function (t) {
-        realms[1].homeTurf = 2;
-        battleTests_1.attackerArmies[18].x = 0;
-        battleTests_1.attackerArmies[18].y = 0;
-        battleTests_1.attackerArmies[18].oldX = -1;
-        battleTests_1.attackerArmies[18].oldY = 0;
-        battleTests_1.defenderArmies[18].x = 0;
-        battleTests_1.defenderArmies[18].y = 0;
-        let battle = new schlacht([battleTests_1.attackerArmies[18]], [battleTests_1.defenderArmies[18]], [], [], 0, 0);
-        t.resultEquals(battle.result(10, 10), { victor: 'attacker', attackerLosses: [720], defenderLosses: [1250] });
+        gameState_1.GameState.realms[0].homeTurf = 2;
+        battleTests_1.attackerArmies[18].changePosition([-1, 0]);
+        battleTests_1.attackerArmies[18].setMovePoints(battleTests_1.attackerArmies[18].getMaxMovePoints());
+        battleTests_1.attackerArmies[18].checkForPossibleMove(2 /* E */);
+        battleTests_1.attackerArmies[18].move(2 /* E */);
+        battleTests_1.defenderArmies[18].changePosition([0, 0]);
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[18]], [battleTests_1.defenderArmies[18]], [], [], [0, 0], 10, 10), new battleResult_1.BattleResult(0 /* ATTACKER_VICTORY */, [720], [1250]));
     });
     test("Combat in defenders home terrain.", function (t) {
-        realms[0].homeTurf = 2;
-        battleTests_1.attackerArmies[18].x = 0;
-        battleTests_1.attackerArmies[18].y = 0;
-        battleTests_1.attackerArmies[18].oldX = -1;
-        battleTests_1.attackerArmies[18].oldY = 0;
-        battleTests_1.defenderArmies[18].x = 0;
-        battleTests_1.defenderArmies[18].y = 0;
-        let battle = new schlacht([battleTests_1.attackerArmies[18]], [battleTests_1.defenderArmies[18]], [], [], 0, 0);
-        t.resultEquals(battle.result(10, 10), { victor: 'defender', attackerLosses: [1250], defenderLosses: [720] });
+        gameState_1.GameState.realms[1].homeTurf = 2;
+        battleTests_1.attackerArmies[18].changePosition([-1, 0]);
+        battleTests_1.attackerArmies[18].setMovePoints(battleTests_1.attackerArmies[18].getMaxMovePoints());
+        battleTests_1.attackerArmies[18].checkForPossibleMove(2 /* E */);
+        battleTests_1.attackerArmies[18].move(2 /* E */);
+        battleTests_1.defenderArmies[18].changePosition([0, 0]);
+        let battleHandler = new battleHandler_1.BattleHandler([], [0, 0]);
+        t.resultEquals(battleHandler.calculateResult([battleTests_1.attackerArmies[18]], [battleTests_1.defenderArmies[18]], [], [], [0, 0], 10, 10), new battleResult_1.BattleResult(2 /* DEFENDER_VICTORY */, [1250], [720]));
     });
 }
 exports.landBattleTests = landBattleTests;
--- unitTests/battleTests/landBattleTests.ts
+++ unitTests/battleTests/landBattleTests.ts
@@ -1,61 +1,84 @@
 import {attackerArmies, defenderArmies} from "../battleTests";
 import {QUnit} from "qunit";
+import {BattleHandler} from "../../armies/battleHandler";
+import {BattleResult, Result} from "../../armies/battleResult";
+import {GameState} from "../../gameState";
+import {Direction} from "../../map/direction";
 
 const { test } = QUnit;
 
 export function landBattleTests() {
     test( "Minimal armies, defenders win by dice roll.", function(t) {
-        let battle = new schlacht([attackerArmies[0]], [defenderArmies[0]], [], [], 0, 0);
-        t.resultEquals( battle.result(1, 10), {victor: 'defender', attackerLosses: [1045], defenderLosses: [861.24]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[0]], [defenderArmies[0]],
+            [], [], [0, 0], 1, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1045], [861.24]));
     });
     test( "Minimal armies, attackers win by dice roll.", function(t) {
-        let battle = new schlacht([attackerArmies[0]], [defenderArmies[0]], [], [], 0, 0);
-        t.resultEquals( battle.result(9, 2), {victor: 'attacker', attackerLosses: [869.57], defenderLosses: [1035]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[0]], [defenderArmies[0]],
+            [], [], [0, 0], 9, 2),
+            new BattleResult(Result.ATTACKER_VICTORY, [869.57], [1035]));
     });
     test( "Minimal armies, tie (both loose).", function(t) {
-        let battle = new schlacht([attackerArmies[0]], [defenderArmies[0]], [], [], 0, 0);
-        t.resultEquals( battle.result(5, 5), {victor: 'tie', attackerLosses: [1000], defenderLosses: [1000]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[0]], [defenderArmies[0]],
+            [], [], [0, 0], 5, 5),
+            new BattleResult(Result.TIE, [1000], [1000]));
     });
     test( "Different officer count, different rolls, tie (both loose).", function(t) {
-        let battle = new schlacht([attackerArmies[0]], [defenderArmies[1]], [], [], 0, 0);
-        t.resultEquals( battle.result(7, 3), {victor: 'tie', attackerLosses: [1000], defenderLosses: [1000]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[0]], [defenderArmies[0]],
+            [], [], [0, 0], 7, 3),
+            new BattleResult(Result.TIE, [1000], [1000]));
     });
     test( "Riders vs footmen on plains.", function(t) {
-        let battle = new schlacht([attackerArmies[1]], [defenderArmies[8]], [], [], 0, 0);
-        t.resultEquals( battle.result(6, 6), {victor: 'defender', attackerLosses: [17000], defenderLosses: [5294.12]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[1]], [defenderArmies[8]],
+            [], [], [0, 0], 6, 6),
+            new BattleResult(Result.DEFENDER_VICTORY, [17000], [5294.12]));
     });
     test( "Riders vs footmen in woods.", function(t) {
-        let battle = new schlacht([attackerArmies[1]], [defenderArmies[8]], [], [], 1, 1);
-        t.resultEquals( battle.result(4, 4), {victor: 'attacker', attackerLosses: [5294.12], defenderLosses: [17000]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [1, 1]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[1]], [defenderArmies[8]],
+            [], [], [1, 1], 4, 4),
+            new BattleResult(Result.ATTACKER_VICTORY, [5294.12], [17000]));
     });
     test( "Mixed armies of different compositions on plains.", function(t) {
-        let battle = new schlacht([attackerArmies[2], attackerArmies[7]], [defenderArmies[2], defenderArmies[7]], [], [], 0, 0);
-        t.resultEquals( battle.result(5, 5), {victor: 'attacker', attackerLosses: [5456.25, 8780.49], defenderLosses: [22687.5, 4395.6]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[2], attackerArmies[7]],
+            [defenderArmies[2], defenderArmies[7]], [], [], [0, 0],
+            5, 5),
+            new BattleResult(Result.ATTACKER_VICTORY, [5456.25, 8780.49], [22687.5, 4395.6]));
     });
     test( "Tie with soldier count, officer count and dice roll all different.", function(t) {
-        let battle = new schlacht([attackerArmies[3]], [defenderArmies[3]], [], [], 0, 0);
-        t.resultEquals( battle.result(1, 10), {victor: 'tie', attackerLosses: [1205], defenderLosses: [995.85]} );
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[3]], [defenderArmies[3]],
+            [], [], [0, 0], 1, 10),
+            new BattleResult(Result.TIE, [1205], [995.85]));
     });
     test( "Combat in attackers home terrain.", function(t) {
-        realms[1].homeTurf = 2;
-        attackerArmies[18].x = 0;
-        attackerArmies[18].y = 0;
-        attackerArmies[18].oldX = -1;
-        attackerArmies[18].oldY = 0;
-        defenderArmies[18].x = 0;
-        defenderArmies[18].y = 0;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 0, 0);
-        t.resultEquals( battle.result(10, 10), {victor: 'attacker', attackerLosses: [720], defenderLosses: [1250]} );
+        GameState.realms[0].homeTurf = 2;
+        attackerArmies[18].changePosition([-1, 0]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([0, 0]);
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [0, 0], 10, 10),
+            new BattleResult(Result.ATTACKER_VICTORY, [720], [1250]));
     });
     test( "Combat in defenders home terrain.", function(t) {
-        realms[0].homeTurf = 2;
-        attackerArmies[18].x = 0;
-        attackerArmies[18].y = 0;
-        attackerArmies[18].oldX = -1;
-        attackerArmies[18].oldY = 0;
-        defenderArmies[18].x = 0;
-        defenderArmies[18].y = 0;
-        let battle = new schlacht([attackerArmies[18]], [defenderArmies[18]], [], [], 0, 0);
-        t.resultEquals( battle.result(10, 10), {victor: 'defender', attackerLosses: [1250], defenderLosses: [720]} );
+        GameState.realms[1].homeTurf = 2;
+        attackerArmies[18].changePosition([-1, 0]);
+        attackerArmies[18].setMovePoints(attackerArmies[18].getMaxMovePoints());
+        attackerArmies[18].checkForPossibleMove(Direction.E);
+        attackerArmies[18].move(Direction.E);
+        defenderArmies[18].changePosition([0, 0]);
+        let battleHandler: BattleHandler = new BattleHandler([], [0, 0]);
+        t.resultEquals( battleHandler.calculateResult([attackerArmies[18]], [defenderArmies[18]],
+            [], [], [0, 0], 10, 10),
+            new BattleResult(Result.DEFENDER_VICTORY, [1250], [720]));
     });
 }
