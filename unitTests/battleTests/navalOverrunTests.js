5360950337cf3427f5ee1af4e2bc426b8ee653cd
Roberts Kolosovs <rk@mbits.solutions>
2018-04-08 10:48:02 +0200

    Refactored test cases in navalOverrunTests. Finished refactoring battleTests.

--- unitTests/battleTests/navalOverrunTests.js
+++ unitTests/battleTests/navalOverrunTests.js
@@ -2,27 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const battleTests_1 = require("../battleTests");
 const qunit_1 = require("qunit");
+const battleHandler_1 = require("../../armies/battleHandler");
 const { test } = qunit_1.QUnit;
 function navalOverrunTests() {
     test("Fleet outnumbered 10:1.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[3]], [battleTests_1.defenderArmies[4]], [], [], 1, 1);
-        t.ok(battle.overrunAttack());
+        let battleHandler = new battleHandler_1.BattleHandler([], [1, 1]);
+        t.ok(battleHandler.calculateResult([battleTests_1.attackerArmies[3]], [battleTests_1.defenderArmies[4]], [], [], [1, 1], 10, 10).result === 1 /* ATTACKER_OVERRUN */);
     });
     test("Defending fleet outnumbered 10:1 despite having warships.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[4]], [battleTests_1.defenderArmies[5]], [], [], 1, 1);
-        t.ok(battle.overrunAttack());
+        let battleHandler = new battleHandler_1.BattleHandler([], [1, 1]);
+        t.ok(battleHandler.calculateResult([battleTests_1.attackerArmies[4]], [battleTests_1.defenderArmies[5]], [], [], [1, 1], 10, 10).result === 1 /* ATTACKER_OVERRUN */);
     });
     test("Guard fleet outnumbering 10:1.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[6]], [battleTests_1.defenderArmies[4]], [], [], 1, 1);
-        t.ok(battle.overrunAttack());
+        let battleHandler = new battleHandler_1.BattleHandler([], [1, 1]);
+        t.ok(battleHandler.calculateResult([battleTests_1.attackerArmies[6]], [battleTests_1.defenderArmies[4]], [], [], [1, 1], 10, 10).result === 1 /* ATTACKER_OVERRUN */);
     });
     test("Guard fleet outnumbered 10:1.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[4]], [battleTests_1.defenderArmies[6]], [], [], 1, 1);
-        t.notOk(battle.overrunAttack());
+        let battleHandler = new battleHandler_1.BattleHandler([], [1, 1]);
+        t.notOk(battleHandler.calculateResult([battleTests_1.attackerArmies[4]], [battleTests_1.defenderArmies[6]], [], [], [1, 1], 10, 10).result === 1 /* ATTACKER_OVERRUN */);
     });
     test("Attacking fleet not outnumbering 10:1 despite having warships.", function (t) {
-        let battle = new schlacht([battleTests_1.attackerArmies[5]], [battleTests_1.defenderArmies[5]], [], [], 1, 1);
-        t.notOk(battle.overrunAttack());
+        let battleHandler = new battleHandler_1.BattleHandler([], [1, 1]);
+        t.notOk(battleHandler.calculateResult([battleTests_1.attackerArmies[5]], [battleTests_1.defenderArmies[5]], [], [], [1, 1], 10, 10).result === 1 /* ATTACKER_OVERRUN */);
     });
 }
 exports.navalOverrunTests = navalOverrunTests;
--- unitTests/battleTests/navalOverrunTests.ts
+++ unitTests/battleTests/navalOverrunTests.ts
@@ -1,27 +1,34 @@
 import {attackerArmies, defenderArmies} from "../battleTests";
 import {QUnit} from "qunit";
+import {BattleHandler} from "../../armies/battleHandler";
+import {Result} from "../../armies/battleResult";
 
 const { test } = QUnit;
 
 export function navalOverrunTests() {
     test( "Fleet outnumbered 10:1.", function(t) {
-        let battle = new schlacht([attackerArmies[3]], [defenderArmies[4]], [], [], 1, 1);
-        t.ok(battle.overrunAttack());
+        let battleHandler: BattleHandler = new BattleHandler([], [1, 1]);
+        t.ok( battleHandler.calculateResult([attackerArmies[3]], [defenderArmies[4]],
+            [], [], [1, 1], 10, 10).result === Result.ATTACKER_OVERRUN );
     });
     test( "Defending fleet outnumbered 10:1 despite having warships.", function(t) {
-        let battle = new schlacht([attackerArmies[4]], [defenderArmies[5]], [], [], 1, 1);
-        t.ok(battle.overrunAttack());
+        let battleHandler: BattleHandler = new BattleHandler([], [1, 1]);
+        t.ok( battleHandler.calculateResult([attackerArmies[4]], [defenderArmies[5]],
+            [], [], [1, 1], 10, 10).result === Result.ATTACKER_OVERRUN );
     });
     test( "Guard fleet outnumbering 10:1.", function(t) {
-        let battle = new schlacht([attackerArmies[6]], [defenderArmies[4]], [], [], 1, 1);
-        t.ok(battle.overrunAttack());
+        let battleHandler: BattleHandler = new BattleHandler([], [1, 1]);
+        t.ok( battleHandler.calculateResult([attackerArmies[6]], [defenderArmies[4]],
+            [], [], [1, 1], 10, 10).result === Result.ATTACKER_OVERRUN );
     });
     test( "Guard fleet outnumbered 10:1.", function(t) {
-        let battle = new schlacht([attackerArmies[4]], [defenderArmies[6]], [], [], 1, 1);
-        t.notOk(battle.overrunAttack());
+        let battleHandler: BattleHandler = new BattleHandler([], [1, 1]);
+        t.notOk( battleHandler.calculateResult([attackerArmies[4]], [defenderArmies[6]],
+            [], [], [1, 1], 10, 10).result === Result.ATTACKER_OVERRUN );
     });
     test( "Attacking fleet not outnumbering 10:1 despite having warships.", function(t) {
-        let battle = new schlacht([attackerArmies[5]], [defenderArmies[5]], [], [], 1, 1);
-        t.notOk(battle.overrunAttack());
+        let battleHandler: BattleHandler = new BattleHandler([], [1, 1]);
+        t.notOk( battleHandler.calculateResult([attackerArmies[5]], [defenderArmies[5]],
+            [], [], [1, 1], 10, 10).result === Result.ATTACKER_OVERRUN );
     });
 }
