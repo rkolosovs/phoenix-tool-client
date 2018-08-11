b94ef7e1f9a0d986ac7c8fe0328e5394f4eff2c8
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2018-08-11 15:14:06 +0200

    Rendering canvas with react.

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 ReactDOM.render(
-<div id={'loginWindow'}>
-<h1>Hello, Welcome to the test page</h1>
-</div>,
-  document.getElementById("root")
+    <div id={'root'}>
+        <canvas id={'canvas'}></canvas>
+        <div id={'loginWindow'}>
+            <h1>Hello, Welcome to my test page</h1>
+        </div>
+    </div>,
+  document.getElementById("body")
 );
