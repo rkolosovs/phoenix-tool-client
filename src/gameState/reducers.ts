6731a86ac15ba3395735607c6b96d6c23908df9a
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2018-08-26 22:22:56 +0200

    Skeleton implementations for all missing reducers (fall through to default).

--- src/gameState/reducers.ts
+++ src/gameState/reducers.ts
@@ -14,9 +14,19 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Phoenixclient.  If not, see <http://www.gnu.org/licenses/>.*/
 
-import {LOG_IN, LOG_OUT, Action, ADD_REALMS, SET_REALMS, REMOVE_REALMS, UPDATE_REALMS} from "./actions";
-import {GameState, initialState, Login, TurnStatus, UserGroup} from "./gameState";
+import {
+    LOG_IN, LOG_OUT, Action, ADD_REALMS, SET_REALMS, REMOVE_REALMS, UPDATE_REALMS, ADD_FIELDS,
+    SET_FIELDS, REMOVE_FIELDS, UPDATE_FIELDS, ADD_RIVERS, SET_RIVERS, REMOVE_RIVERS, ADD_ARMIES, SET_ARMIES,
+    REMOVE_ARMIES, UPDATE_ARMIES, ADD_BUILDINGS, SET_BUILDINGS, REMOVE_BUILDINGS, UPDATE_BUILDINGS, ADD_NEW_EVENTS,
+    SET_NEW_EVENTS, REMOVE_NEW_EVENTS, UPDATE_NEW_EVENTS, SET_LOADED_EVENTS, UPDATE_LOADED_EVENTS, SET_CURRENT_TURN
+} from "./actions";
+import {GameState, initialState, Login, Turn, TurnStatus, UserGroup} from "./gameState";
 import {Realm} from "../model/realm";
+import {Field} from "../model/map/field";
+import {River} from "../model/map/river";
+import {Army} from "../model/armies/army";
+import {Building} from "../model/buildings/building";
+import {PhoenixEvent} from "../model/events/event";
 
 function loginReducer (state: Login = {name: 'stranger', group: UserGroup.GUEST, realm: undefined}, action: Action<any>) {
     switch (action.type) {
@@ -61,16 +71,87 @@ function realmsReducer (state: Realm[] = [], action: Action<any>) {
     }
 }
 
+function fieldsReducer (state: Field[] = [], action: Action<any>) {
+    switch (action.type) {
+        case ADD_FIELDS:
+        case SET_FIELDS:
+        case REMOVE_FIELDS:
+        case UPDATE_FIELDS:
+        default:
+            return state;
+    }
+}
+
+function riversReducer (state: River[] = [], action: Action<any>) {
+    switch (action.type) {
+        case ADD_RIVERS:
+        case SET_RIVERS:
+        case REMOVE_RIVERS:
+        default:
+            return state;
+    }
+}
+
+function armiesReducer (state: Army[] = [], action: Action<any>) {
+    switch (action.type) {
+        case ADD_ARMIES:
+        case SET_ARMIES:
+        case REMOVE_ARMIES:
+        case UPDATE_ARMIES:
+        default:
+            return state;
+    }
+}
+
+function buildingsReducer (state: Building[] = [], action: Action<any>) {
+    switch (action.type) {
+        case ADD_BUILDINGS:
+        case SET_BUILDINGS:
+        case REMOVE_BUILDINGS:
+        case UPDATE_BUILDINGS:
+        default:
+            return state;
+    }
+}
+
+function newEventsReducer (state: PhoenixEvent[] = [], action: Action<any>) {
+    switch (action.type) {
+        case ADD_NEW_EVENTS:
+        case SET_NEW_EVENTS:
+        case REMOVE_NEW_EVENTS:
+        case UPDATE_NEW_EVENTS:
+        default:
+            return state;
+    }
+}
+
+function loadedEventsReducer (state: PhoenixEvent[] = [], action: Action<any>) {
+    switch (action.type) {
+        case SET_LOADED_EVENTS:
+        case UPDATE_LOADED_EVENTS:
+        default:
+            return state;
+    }
+}
+
+function currentTurnReducer (state: Turn = {turn: 0, realm: "sl", status: TurnStatus.STARTED}, action: Action<any>) {
+    switch (action.type) {
+        case SET_CURRENT_TURN:
+        default:
+            return state;
+    }
+}
+
 export default (state: GameState = initialState, action: Action<any>): GameState => {
     return {
         realms: realmsReducer(state.realms, action),
-        fields: [],
-        rivers: [],
-        armies: [],
-        buildings: [],
-        newEvents: [],
-        loadedEvents: [],
-        currentTurn: {turn: 0, realm: "sl", status: TurnStatus.STARTED},
+        fields: fieldsReducer(state.fields, action),
+        rivers: riversReducer(state.rivers, action),
+        armies: armiesReducer(state.armies, action),
+        buildings: buildingsReducer(state.buildings, action),
+        newEvents: newEventsReducer(state.newEvents, action),
+        loadedEvents: loadedEventsReducer(state.loadedEvents, action),
+        currentTurn: currentTurnReducer(state.currentTurn, action),
         login: loginReducer(state.login, action)
     };
 }
