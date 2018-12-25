cb384d9e9539e83d534ba29671befa205be982a2
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2018-12-25 16:46:03 +0100

    Function for loading realms

--- src/serverInteraction/loadingFunctions.ts
+++ src/serverInteraction/loadingFunctions.ts
@@ -14,12 +14,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Phoenixclient.  If not, see <http://www.gnu.org/licenses/>.*/
 
-import {initialState, Turn} from "../gameState/gameState";
+import {Turn} from "../gameState/gameState";
 import config from "../../config/config";
 import {XMLHttpRequest} from "xmlhttprequest";
+import {Realm} from "../model/realm";
 
-export async function loadCurrentTurn(retries: number = 0): Promise<Turn> {
-    console.log("Loading current turn. Attempt ", retries);
+function loadAny(message: string, urlSuffix: string, retries: number = 0): Promise<any> {
+    console.log(message, retries);
     return new Promise<Turn>((resolve, reject) => {
         let xhr = new XMLHttpRequest();
 
@@ -31,16 +32,26 @@ export async function loadCurrentTurn(retries: number = 0): Promise<Turn> {
             else if (xhr.readyState === 4) {
                 console.log('Request failed.  Returned status of ' + this.status);
                 if (this.status === 0 && retries < config.max_retries){
-                    setTimeout(() => loadCurrentTurn(retries+1), 100);
+                    setTimeout(() => loadAny(message, urlSuffix,retries+1), 100);
                 } else {
-                    reject(initialState.currentTurn);
+                    reject();
                 }
             } else {
                 console.log('Waiting for request to be done. Current ready state is ' + xhr.readyState);
             }
         };
 
-        xhr.open('GET', config.server_url + "/databaseLink/getturn/");
+        xhr.open('GET', config.server_url + urlSuffix);
         xhr.send();
     });
+}
+
+export async function loadCurrentTurn(): Promise<Turn> {
+    return (loadAny("Loading current turn. Attempt ",
+        "/databaseLink/getturn/") as Promise<Turn>);
+}
+
+export async function loadRealms(): Promise<Realm[]> {
+    return (loadAny("Loading realms. Attempt ",
+        "/databaseLink/getrealms/") as Promise<Realm[]>);
 }
